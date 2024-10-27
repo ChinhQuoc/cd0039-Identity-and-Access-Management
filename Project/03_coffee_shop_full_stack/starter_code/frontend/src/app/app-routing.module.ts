8f05f61b62f539b7920b805d527bebc6bb37c5b6
DESKTOP-RNK2HL9\DELL <dthiep.gm@gmail.com>
2024-10-28 01:37:32 +0700

    implement authen and autho

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/app/app-routing.module.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuardService } from "./services/auth-guard.service";
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  {
+    path: "",
+    loadChildren: "./pages/tabs/tabs.module#TabsPageModule",
+    canActivate: [AuthGuardService],
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    url: "dev-5l37br2fze6hxdk6.us", // the auth0 domain prefix
+    audience: "coffee", // the audience set for the auth0 app
+    clientId: "9KGkToYHqbjbLDjoHJLS6AmTYU592ghY", // the client id generated for the auth0 app
+    callbackURL: "http://localhost:8100/tabs/drink-menu", // the base url of the running ionic application.
+  },
 };
